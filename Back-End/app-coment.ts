038c3ea60067d7d7942f886dba5afd1959a9b1f1
RNanWP <renan92011@hotmail.com>
2025-07-06 18:46:35 -0300

    Adicionando as dependencias necessárias

---
+++ Back-End/app-coment.ts
@@ -0,0 +1,16 @@
+// Connect to Cluster0 Tech Challenge
+//
+// reenan92011
+// uTUCcf8O3A2bB8P1
+
+//          COISAS A SE FAZER NO PROJETO:
+// 
+// Pegar o JWT Token e implementar-lo no código
+
+
+//          DICAS E APRENDIZADOS
+// 
+// .ENV usado para adicionar a rota do MongoDB. 
+// Ex:PORT=3000
+// MONGO_URI=mongodb://mongo:27017/blogdb
+// JWT_SECRET=seuSegredoAqui
