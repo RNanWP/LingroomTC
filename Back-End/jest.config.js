2e1137e9192a349b19437134dc80fac007842a49
RNanWP <renan92011@hotmail.com>
2025-07-15 20:53:03 -0300

    Feat: Feat: Subindo pasta git para o repo, configurando tsconfig e jest

--- Back-End/jest.config.js
+++ Back-End/jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-
   testMatch: ["**/src/**/*.test.ts"],
-  // Ignora a pasta de build 'dist'
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  setupFilesAfterEnv: ["./jest.setup.ts"],
 };
