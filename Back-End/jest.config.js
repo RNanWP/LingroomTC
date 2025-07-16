5bd41fb7b462e4045d49f6627351bb06b2ac1892
RNanWP <renan92011@hotmail.com>
2025-07-15 21:12:22 -0300

    Feat: Arrumando a pasta dist, jest, gitignore

--- Back-End/jest.config.js
+++ Back-End/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
+
   testMatch: ["**/src/**/*.test.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  setupFilesAfterEnv: ["./jest.setup.ts"],
 };
