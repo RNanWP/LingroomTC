df655a54d7f0be0b9b39063f766d7e89a88a6729
RNanWP <renan92011@hotmail.com>
2025-07-20 20:20:58 -0300

    Feat: Atualizando server.ts para o Render

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -1,27 +1,43 @@
-console.log("--- INICIANDO SERVIDOR: Verificando Variáveis de Ambiente ---");
-console.log(
-  `MONGO_URI: ${process.env.MONGO_URI ? "Definida" : "NÃO DEFINIDA"}`
-);
-console.log(
-  `JWT_SECRET: ${process.env.JWT_SECRET ? "Definido" : "NÃO DEFINIDO"}`
-);
-console.log(`PORT: ${process.env.PORT}`);
-console.log("----------------------------------------------------------");
-
 import mongoose from "mongoose";
 import { app } from "./app";
 import { MONGO_URI, PORT } from "./config";
 
-if (process.env.NODE_ENV !== "test") {
-  mongoose
-    .connect(MONGO_URI)
-    .then(() => {
-      console.log("MongoDB conectado");
-      app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
-      });
-    })
-    .catch((err) => console.error("Falha ao conectar ao MongoDB", err));
-}
+const startServer = async () => {
+  if (!MONGO_URI) {
+    console.error(
+      "ERRO FATAL: A variável de ambiente MONGO_URI não está definida."
+    );
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB conectado com sucesso!");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Falha ao conectar ao MongoDB", err);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 export { app };
+
+// --------------------------------------
+// if (process.env.NODE_ENV !== "test") {
+//   mongoose
+//     .connect(MONGO_URI)
+//     .then(() => {
+//       console.log("MongoDB conectado");
+//       app.listen(PORT, () => {
+//         console.log(`Servidor rodando na porta ${PORT}`);
+//       });
+//     })
+//     .catch((err) => console.error("Falha ao conectar ao MongoDB", err));
+// }
+
+// export { app };
