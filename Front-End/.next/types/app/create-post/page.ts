29a13b4c4c4f88818ad9ec55a274744ce6f153ff
RNanWP <renan92011@hotmail.com>
2025-10-02 22:15:14 -0300

    feat(edit-post): permitir trocar imagem de capa; util toImgUrl com base na API; permitir domínios de imagens

--- Front-End/.next/types/app/create-post/page.ts
+++ Front-End/.next/types/app/create-post/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\renan\OneDrive\Documentos\FACULDADE\LingroomTC\front-end\src\app\create-post\page.tsx
+// File: C:\Users\renan\OneDrive\Documentos\FACULDADE\LingroomTC\Front-End\src\app\create-post\page.tsx
 import * as entry from '../../../../src/app/create-post/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
