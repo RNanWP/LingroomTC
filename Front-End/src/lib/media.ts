5227fda47e50731ec41887dbf65c4d0a263b5b20
RNanWP <renan92011@hotmail.com>
2025-10-07 23:14:32 -0300

    Feat: Arrumando pra tentar deploy no Vercel

--- Front-End/src/lib/media.ts
+++ Front-End/src/lib/media.ts
@@ -1,5 +1,5 @@
 export const API_BASE = (
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000"
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api"
 ).replace(/\/+$/, "");
 
 export function toImgUrl(pathOrUrl?: string) {
